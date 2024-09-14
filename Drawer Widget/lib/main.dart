27ff138049b45abe7dbf570ef20d028eea468115
Hamza Tariq <connect.hamzatariq@gmail.com>
2024-09-14 23:06:40 +0500

    Add files via upload
    Hey guys ! This is the Source Code for the Drawer Widget I explained in the YouTube Video.

---
+++ Drawer Widget/lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:studioxd_notesapp/pages/homepage.dart';
+import 'package:studioxd_notesapp/pages/secondpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    routes: {
+      'secondpage' : (context) => Secondpage(),
+    },
+    );
+    
+  }
+}
